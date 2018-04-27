6edd18e11f40dcdb35b1fa3d68fb265e2e34b75c
leiyangs <472093743@qq.com>
2018-04-27 18:12:36 +0800

    page_object

--- test/test_baidu.py
+++ test/test_baidu.py
@@ -1,38 +1,33 @@
 import time
 import unittest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from utils.config import Config, DRIVER_PATH, DATA_PATH, REPORT_PATH
+from utils.config import Config, DATA_PATH, REPORT_PATH
 from utils.log import logger
 from utils.file_reader import ExcelReader
 from utils.HTMLTestRunner import HTMLTestRunner
 from utils.mail import Email
+from test.page.baidu_result_page import BaiDuMainPage, BaiDuResultPage
 
 
 class TestBaiDu(unittest.TestCase):
     URL = Config().get('URL')
     excel = DATA_PATH + '/baidu.xlsx'
 
-    locator_kw = (By.ID, 'kw')
-    locator_su = (By.ID, 'su')
-    locator_result = (By.XPATH, '//div[contains(@class, "result")]/h3/a')
-
     def sub_setUp(self):
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH + '\chromedriver.exe')
-        self.driver.get(self.URL)
+        # 初始页面是main page，传入浏览器类型打开浏览器
+        self.page = BaiDuMainPage(browser_type='chrome').get(self.URL, maximize_window=False)
 
     def sub_tearDown(self):
-        self.driver.quit()
+        self.page.quit()
 
     def test_search(self):
         datas = ExcelReader(self.excel).data
         for d in datas:
             with self.subTest(data=d):
                 self.sub_setUp()
-                self.driver.find_element(*self.locator_kw).send_keys(d['search'])
-                self.driver.find_element(*self.locator_su).click()
+                self.page.search(d['search'])
                 time.sleep(2)
-                links = self.driver.find_elements(*self.locator_result)
+                self.page = BaiDuResultPage(self.page)  # 页面跳转到result page
+                links = self.page.result_links
                 for link in links:
                     logger.info(link.text)
                 self.sub_tearDown()
@@ -43,12 +38,3 @@ if __name__ == '__main__':
     with open(report, 'wb') as f:
         runner = HTMLTestRunner(f, verbosity=2, title='从0搭建测试框架 灰蓝', description='修改html报告')
         runner.run(TestBaiDu('test_search'))
-    e = Email(title='百度搜索测试报告',
-              message='这是今天的测试报告，请查收！',
-              receiver='...',
-              server='...',
-              sender='...',
-              password='...',
-              path=report
-              )
-    e.send()
